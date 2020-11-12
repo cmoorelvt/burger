7bc78a4b55402e8e9ece44ed65a0bc2dc3b1016a
Christine Moore <cmoorelvt@gmail.com>
2020-11-12 10:42:25 -0500

    set up sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Kahuna", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dante's Inferno", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("White Castle", true);
